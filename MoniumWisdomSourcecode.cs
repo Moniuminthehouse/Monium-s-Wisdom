e7cd7e77c8d9f867239ae12188ee0082c678d932
Moniuminthehouse <wizardpearisherelol@gmail.com>
2025-03-12 22:46:36 +0000

    Add files via upload

---
+++ MoniumWisdomSourcecode.cs
@@ -0,0 +1,119 @@
+using System.ComponentModel;
+
+namespace Moniums_Wisdom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            // Load image from file
+            this.BackgroundImage = Image.FromFile(@"C:\Users\souha\OneDrive\Images\Catfrom.png");
+
+            // Set layout (optional)
+            this.BackgroundImageLayout = ImageLayout.Stretch; // Options: Tile, Center, Zoom, None
+
+
+        }
+
+        private void Wisdom_Click(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+            int thyrandom = random.Next(1, 10);
+
+
+            switch (thyrandom)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        label2.Text = string.Format("As trolly once said : indians are hatin on me");
+                        break;
+                    }
+                case 2:
+                    {
+                        label2.Text = string.Format("Winston churchill more like church is ill win he is ston");
+                        break;
+                    }
+                case 3:
+                    {
+                        label2.Text = string.Format("It has to be stated that the phone must be given to the man");
+                        break;
+                    }
+                case 4:
+                    {
+                        label2.Text = string.Format("This is monium's first app!");
+                        break;
+                    }
+                case 5:
+                    {
+                        label2.Text = string.Format("mate go study you have a math test tomorrow");
+                        break;
+                    }
+                case 6:
+                    {
+                        label2.Text = string.Format("CS majors on their way to the homeless shelter");
+                        break;
+                    }
+                case 7:
+                    {
+                        label2.Text = string.Format("Hi!");
+                        break;
+                    }
+                case 8:
+                    {
+                        label2.Text = string.Format("Fun fact : monium's first cat was nothing!");
+                        break;
+                    }
+                case 9:
+                    {
+                        label2.Text = string.Format("The final message! (for now)");
+                        break;
+                    }
+            }
+        }
+
+        private void Randomnumber_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randoman = random.Next(0, 100);
+            label1.Text = string.Format("Rosco Says {0}!", randoman);
+
+            if (randoman == 0)
+            {
+                label1.Text = string.Format("Your day is extremely bad... probably");
+            }
+            else if (randoman == 99)
+            {
+                label1.Text = string.Format("{0}!!!!!", randoman);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is the first application made by monium! it includes 2 buttons one that has 10 messages for the users and one that picks a number between 1 - 100 send feedback to my instagram (moniuminthehouse)");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
